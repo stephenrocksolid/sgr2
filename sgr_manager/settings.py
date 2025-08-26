80537b6ac80a7a2f041f495b3d7395e286e89b9f
Stephen Aguilar <stephen@rocksoliddata.solutions>
2025-08-26 12:05:04 -0600

    Allow sgr.rocksoliddata.solutions in ALLOWED_HOSTS

--- sgr_manager/settings.py
+++ sgr_manager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-+@r1$)fv(rc5+q@dy3se7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1', 'sgr.rocksoliddata.solutions').split(',')
 
 
 # Application definition
